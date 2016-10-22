b8dac4cb6c384e17c0cee3aa0068c0b8fe365ea4
Tiago Natel de Moura <tiago.natel@neoway.com.br>
2016-10-22 00:43:56 -0200

    16 bit real mode C (#4)
    * C version using BootProg and SmallerC
    
    * Add dependencies to makefile
    
    * test VESA VBE info
    
    * add vesa vbe struct
    
    Signed-off-by: Tiago Natel de Moura <tiago.natel@neoway.com.br>

---
+++ Makefile
@@ -0,0 +1,32 @@
+.PHONY: clean image run
+
+OSBIN = src/startup.bin
+IMG   = flp144.img
+
+all: clean deps build image run
+
+deps: scripts/BootProg/mkimg144 scripts/BootProg/flp144.bin
+
+scripts/BootProg/flp144.bin: scripts/BootProg
+	cd scripts/BootProg && nasm -f bin flp144.asm -o flp144.bin
+
+scripts/BootProg/mkimg144: scripts/BootProg
+	cd scripts/BootProg && smlrcc mkimg144.c -o mkimg144
+
+scripts/BootProg:
+	mkdir -p scripts
+	cd scripts && git clone git@github.com:alexfru/BootProg.git
+
+build: $(OSBIN)
+
+$(OSBIN):
+	cd src && make build
+
+image: $(OSBIN)
+	./scripts/BootProg/mkimg144 -bs ./scripts/BootProg/flp144.bin -o $(IMG) -us src/startup.bin
+
+run: $(IMG)
+	qemu-system-i386 -fda $(IMG) -m 128
+
+clean:
+	rm -f src/main.asm src/startup.bin
---
+++ src/include/EnzOS/vesa.h
@@ -0,0 +1,38 @@
+#ifndef VESA_H
+#define VESA_H
+
+// video mode, video buffer segment and dimensions
+#define VMODE   3 // 40x25 color text mode
+#define VSEG    0xB800
+#define VWIDTH  80
+#define VHEIGHT 25
+
+// foreground and background colors
+#define FORE_BLACK   0x00
+#define FORE_BLUE    0x01
+#define FORE_GREEN   0x02
+#define FORE_CYAN    0x03
+#define FORE_RED     0x04
+#define FORE_MAGENTA 0x05
+#define FORE_BROWN   0x06
+#define FORE_WHITE   0x07
+#define FORE_GRAY           0x08
+#define FORE_BRIGHT_BLUE    0x09
+#define FORE_BRIGHT_GREEN   0x0A
+#define FORE_BRIGHT_CYAN    0x0B
+#define FORE_BRIGHT_RED     0x0C
+#define FORE_BRIGHT_MAGENTA 0x0D
+#define FORE_YELLOW         0x0E
+#define FORE_BRIGHT_WHITE   0x0F
+#define BACK_BLACK   0x00
+#define BACK_BLUE    0x10
+#define BACK_GREEN   0x20
+#define BACK_CYAN    0x30
+#define BACK_RED     0x40
+#define BACK_MAGENTA 0x50
+#define BACK_BROWN   0x60
+#define BACK_WHITE   0x70
+
+#define TEXTCOLOR (BACK_BLACK | FORE_BRIGHT_WHITE)
+
+#endif
